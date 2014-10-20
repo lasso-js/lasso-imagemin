0df3954c0bc9927be42b809c00003af5fb173cc5
Patrick Steele-Idem <pnidem@gmail.com>
2014-10-20 16:14:32 -0600

    Removed misleading comment

--- optimizer-imagemin-plugin.js
+++ optimizer-imagemin-plugin.js
@@ -15,17 +15,6 @@ module.exports = function (pageOptimizer, pluginConfig) {
         stream: true,
 
         transform: function(inStream, optimizerContext) {
-
-            /*
-            What this method will do:
-            1. Convert the incoming stream to a buffer
-            2. Calculate a hash of the buffer to use for the output file name
-            3. If the output file already exists in the destination directory then return the input stream for that file
-            4. Else:
-                a. configure imagemin based on the file type
-                b. minify the image
-                c. return a a file read stream for the optimized image written to disk
-             */
              var path = optimizerContext.path;
              ok(typeof path === 'string', 'Path expected');
              return minifier.minify(inStream, path, optimizerContext);

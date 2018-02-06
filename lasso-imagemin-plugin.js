71f6dbc9881025dcc9f86282bfa3d8fec0e9b165
Austin Kelleher <austin.kell47@gmail.com>
2018-02-06 11:31:53 -0500

    Add safety check to ensure that only known image extensions get handled.

--- lasso-imagemin-plugin.js
+++ lasso-imagemin-plugin.js
@@ -1,8 +1,7 @@
-var ok = require('assert').ok;
+var nodePath = require('path');
 var Minifier = require('./Minifier');
 
 module.exports = function (lasso, pluginConfig) {
-
     var types = pluginConfig.types || ['gif', 'jpeg', 'jpg', 'png', 'svg'];
 
     var minifier = new Minifier(pluginConfig);
@@ -15,8 +14,14 @@ module.exports = function (lasso, pluginConfig) {
         stream: true,
 
         transform: function(inStream, lassoContext) {
-             var path = lassoContext.path;
-             return typeof path === 'string' ? minifier.minify(inStream, path, lassoContext) : inStream;
+            var path = lassoContext.path;
+
+            if (typeof path === 'string') {
+                var ext = nodePath.extname(path).substr(1);
+                return types.indexOf(ext) !== -1 ? minifier.minify(inStream, path, lassoContext) : inStream;
+            } else {
+                return inStream;
+            }
         }
     });
 };
